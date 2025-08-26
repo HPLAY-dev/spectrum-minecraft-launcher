469d238c741d40dab8da88c078fd5e22be3e90d8
HPLAY-dev <harryq2020@163.com>
2025-08-26 16:25:47 +0800

    Update make

--- make.bat
+++ make.bat
@@ -1,10 +1,13 @@
 set PROJECT=main
 set PYINSTALLER=C:\Users\magic\Documents\Programs\python310\Scripts\pyinstaller.exe
+set 7ZFILENAMEPREFIX=build
+set 7ZFILENAMESUFFIX=py310-x86_64
+set /p VERSION=Version: 
 
 :Environment
 if exist .\build rd /s /q .\build
 if not exist .\builds mkdir builds
-if not exist .\builds\build mkdir .\builds\build
+if not exist .\builds\build-%VERSION% mkdir .\builds\build-%VERSION%
 mkdir build
 
 :BUILD
@@ -13,6 +16,6 @@ cd build
 xcopy .\dist .\..\builds\build /I /Q
 
 :ARCHIVE
-7z a -mx0 ".\..\builds\latest.7z" ".\..\builds\build\*"
+7z a -mx0 ".\..\builds\%7zFilenamePrefix%-%Version%-%7zFilenameSuffix%.7z" ".\..\builds\build\*"
 
 cd ..
