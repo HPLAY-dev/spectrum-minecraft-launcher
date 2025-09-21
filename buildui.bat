01d5a0dcc7550a75c21759768a1a3f0a8f90e458
HPLAY-dev <harryq2020@163.com>
2025-09-21 20:02:13 +0800

    update README and stuff

--- buildui.bat
+++ buildui.bat
@@ -1,1 +1,1 @@
-C:\Users\magic\Documents\Programs\python310\Scripts\pyuic5.exe -o c:\Users\magic\Documents\projects\LauncherX\ui.py c:\Users\magic\Documents\projects\LauncherX\qt.ui
+pyuic5 -o .\ui.py .\qt.ui
