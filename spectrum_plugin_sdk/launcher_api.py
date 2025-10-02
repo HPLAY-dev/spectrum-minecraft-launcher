9f11b3a15e0bb0f1654734beece71c979c915c36
HPLAY-dev <harryq2020@163.com>
2025-10-02 20:43:54 +0800

    remove wrong-uploaded files

--- spectrum_plugin_sdk/launcher_api.py
+++
@@ -1,9 +0,0 @@
-class LauncherAPI:
-    def __init__(self, launcher):
-        self.launcher = launcher
-
-    def log_info(self, msg):
-        print(f"[Plugin][INFO] {msg}")
-
-    def log_error(self, msg):
-        print(f"[Plugin][ERROR] {msg}")
