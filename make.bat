b72897c4beb74b75431476249d7013e5912260a3
HPLAY-dev <harryq2020@163.com>
2025-10-03 12:35:45 +0800

    update make.bat

--- make.bat
+++ make.bat
@@ -1,33 +1,55 @@
+@echo off
+
+:: Check what to do
 if "%1"=="clean" goto clean
+if "%1"=="build" goto BUILD_ADD
+if "%1"=="7z" goto ARCHIVE
+goto HELP
 
-set PROJECT=main
-set PYINSTALLER=pyinstaller
-set PREFIX=build
-set SUFFIX=python-x86_64
-set /p VERSION=Version: 
+:BUILD_ALL
+	:Environment
+	echo [make] Setting up environment
+	set PROJECT=main
+	set PYINSTALLER=pyinstaller
+	set PREFIX=build
+	set SUFFIX=python-x86_64
+	set 7z=7z
+	set /p VERSION=Version: 
+	
+	:CLEAN
+	if exist .\build rd /s /q .\build
+	if not exist .\builds mkdir builds
+	if not exist .\builds\build-%VERSION% mkdir .\builds\build-%VERSION%
+	mkdir build
 
-:Environment
-if exist .\build rd /s /q .\build
-if not exist .\builds mkdir builds
-if not exist .\builds\build-%VERSION% mkdir .\builds\build-%VERSION%
-mkdir build
+	:BUILD
+	cd build
+	rem %pyinstaller% --noconfirm --onedir --windowed .\..\%PROJECT%.py
+	%pyinstaller% --noconfirm --onedir .\..\%PROJECT%.py
+	xcopy .\dist\%PROJECT% .\..\builds\build-%Version% /E /I /Q
+	mkdir .\..\builds\build-%Version%\assets
+	xcopy .\..\assets .\..\builds\build-%Version%\assets /E /I /Q
 
-:BUILD
-cd build
-rem %pyinstaller% --noconfirm --onedir --windowed .\..\%PROJECT%.py
-%pyinstaller% --noconfirm --onedir .\..\%PROJECT%.py
-xcopy .\dist\%PROJECT% .\..\builds\build-%Version% /E /I /Q
-mkdir .\..\builds\build-%Version%\assets
-xcopy .\..\assets .\..\builds\build-%Version%\assets /E /I /Q
-
+	cd ..
+	goto EOF
+	
+	
 :ARCHIVE
-7z a -mx0 .\..\builds\%Prefix%-%Version%-%Suffix%.7z .\..\builds\build-%Version%\*
-
-cd ..
-goto EOF
+	%7z% a -mx0 .\..\builds\%Prefix%-%Version%-%Suffix%.7z .\..\builds\build-%Version%\*
 
 :clean
-rd /s /q .\build
-goto EOF
+	rd /s /q .\build
+	goto EOF
 
+:HELP
+	echo Make batfile for spectrum launcher
+	echo generates binaries from pyinstaller and move it to ./builds/build-VERSION
+	echo.
+	echo     Usage: ./make clean  - Clean build folder
+	echo     Usage: ./make build  - Build via PyInstaller
+	echo     Usage: ./make 7z     - Create 7z archive for distributing (7z needed)
+	echo     Usage: ./make help   - Display this infomation
+	echo.
+	echo to specify binaries path, edit this batch file.
+	goto EOF
 :EOF
